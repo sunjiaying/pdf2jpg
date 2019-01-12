a72c0ae6e3d315ef6b545bf404f816075ad5bf38
sunjiaying <sunjoin@qq.com>
2019-01-13 02:28:29 +0800

    支持linux命令

--- main.go
+++ main.go
@@ -3,40 +3,41 @@ package main
 import (
 	"flag"
 	"fmt"
-	"runtime"
-	"os/exec"
 	"io/ioutil"
+	"os/exec"
 	"path"
+	"runtime"
 	"time"
 )
 
-func main(){
+func main() {
 	pdffilepath := flag.String("pdf", "", "Input PDF Filenpath")
 	outputpath := flag.String("output", "", "Output path")
 
 	flag.Parse()
-	
+
 	fmt.Println("pdffilepath:", *pdffilepath)
 	fmt.Println("outputpath:", *outputpath)
 
 	t1 := time.Now()
 	if runtime.GOOS == "windows" {
-		cmd := exec.Command("magick.exe", "convert", "-density", "200", *pdffilepath, *outputpath + "image-%03d.jpg")
+		cmd := exec.Command("magick.exe", "convert", "-density", "200", *pdffilepath, *outputpath+"image-%03d.jpg")
 
 		out, err := cmd.Output()
-		if err != nil {  
+		if err != nil {
 			fmt.Println(err)
 		}
 		fmt.Println(string(out))
 		listJpgs(*outputpath)
 	} else {
-		cmd := exec.Command("convert", "-v")
+		cmd := exec.Command("convert", "-verbose", "-density", "200", *pdffilepath, *outputpath+"image-%03d.jpg")
 
 		out, err := cmd.Output()
-		if err != nil {  
-				fmt.Println(err)  
-		}  
+		if err != nil {
+			fmt.Println(err)
+		}
 		fmt.Println(string(out))
+		listJpgs(*outputpath)
 	}
 
 	t2 := time.Now()
@@ -47,9 +48,9 @@ func main(){
 func listJpgs(pathname string) error {
 	rd, err := ioutil.ReadDir(pathname)
 	for _, fi := range rd {
-			if path.Ext(fi.Name()) == ".jpg" {
-					fmt.Printf("%s\n", fi.Name())
-			}
+		if path.Ext(fi.Name()) == ".jpg" {
+			fmt.Printf("%s\n", fi.Name())
+		}
 	}
 	return err
 }
